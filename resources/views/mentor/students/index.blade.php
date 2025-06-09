b5281f9b1afd3129160ebf3eea563e5d392880cc
jeanpierre <jeanpinie13998ef@linkiafp.online>
2025-06-09 19:29:05 +0200

    Actualización proyecto

--- resources/views/mentor/students/index.blade.php
+++ resources/views/mentor/students/index.blade.php
@@ -224,14 +224,6 @@
             </svg>
             <h3 class="mt-2 text-sm font-medium text-gray-900">Aún no tienes estudiantes asignados</h3>
             <p class="mt-1 text-sm text-gray-500">Cuando se te asignen estudiantes, aparecerán en esta lista.</p>
-            <div class="mt-6">
-                <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                    <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-                    </svg>
-                    Invitar Estudiante
-                </a>
-            </div>
         </div>
         @endif
     </div>
